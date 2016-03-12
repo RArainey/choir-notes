9c0579d3765b156700e71f8479834e309ff44e07
Ryan Rainey <ryanrainey@zoho.com>
2016-03-11 19:10:24 -0800

    removed background img, replaced with img inside note-area divs

--- app/components/staff-column.js
+++ app/components/staff-column.js
@@ -5,19 +5,29 @@ export default Ember.Component.extend({
 	click(e) {
 		let target = e.target;
 		let index = $(target).index();
+		let note;
 
-		if (this.domIndex == null) {
-			let note = document.createElement("img");
+		// Create the note.
+		if (this.domIndex === null) {
+			note = document.createElement("img");
 			note.src = 'assets/images/note.png';
-			
+			note.className = 'note';
 			target.appendChild(note);
 			this.domIndex = index;
 
-		} else if (target.tagName != "IMG") {
-			let note = target.parentElement.children[this.domIndex].children[0];
-
+		// User tapped/clicked elsewhere, move the note.
+		} else if (target.tagName === "DIV" && this.domIndex !== index) {
+			note = target.parentElement.children[this.domIndex].children[0];
 			target.appendChild(note);
 			this.domIndex = index;
+
+		// User tapped/clicked the note or its location, remove it.
+		} else if (target.tagName === "DIV" && this.domIndex === index) {
+			target.children[0].remove();
+			this.domIndex = null;
+		} else if (target.tagName === "IMG") {
+			target.remove();
+			this.domIndex = null;
 		}
 	}
 });
